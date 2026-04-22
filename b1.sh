fabcc3ec12c73e8f2b34b3288bbe4312fd43d19e
santosg572 <santosg572@gmail.com>
2026-04-22 07:26:18 -0600

    Test comité

---
+++ b1.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+matlab -nodesktop -nosplash -r "conn_batch('my_config.mat');"
+
+#Batch Structure: The core of Linux automation is the conn_batch command, which takes a MATLAB structure 
+#(saved in a .mat file) containing all parameters (e.g., batch.Setup, batch.Analysis).
+
+https://andysbrainbook.readthedocs.io/en/latest/FunctionalConnectivity/CONN_ShortCourse/CONN_12_Scripting.html
+
+https://github.com/laurabravo97
+
+https://andysbrainbook.readthedocs.io/en/stable/FunctionalConnectivity/CONN_ShortCourse/CONN_04_GUI_Overview.html
+
+https://andysbrainbook.readthedocs.io/en/latest/FunctionalConnectivity/CONN_Overview.html
+
+
+
